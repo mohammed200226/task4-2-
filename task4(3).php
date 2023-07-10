83bd3c4e99907b07beec14afe0d8b46e560a576d
mohammed200226 <133527086+mohammed200226@users.noreply.github.com>
2023-07-11 00:23:06 +0300

    Create task4(3).php

---
+++ task4(3).php
@@ -0,0 +1,9 @@
+<?php
+
+$products = [
+    'prod1' => ['name' => 'a', 'price' => 300, 'clr' => 'green'],
+    'prod2' => ['name' => 'b', 'price' => 550, 'clr' => 'yellow'],
+    'prod3' => ['name' => 'w', 'price' => 100, 'clr' => 'blue'],
+    'prod4' => ['name' => 'f', 'price' => 280, 'clr' => 'pink'],
+];
+?>
